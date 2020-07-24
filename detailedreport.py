178b8cd442c94c593940425ba4d883a60f2cee01
Jason <japhillips@veracode.com>
2020-07-24 18:46:37 -0400

    Fixed bug in flaw file path

--- detailedreport.py
+++ detailedreport.py
@@ -19,49 +19,57 @@ def getJSONdata():
 		#
 		# Capturing Data from JSON results file and saving to dictionary
 		#
-		for i in data['results']['TestResults']['Issues']['Issue']:
-			title=i['Title']
-			issueid=i['IssueId']
-			severity=i['Severity']
-			issuetype=i['IssueType']
-			cweid=i['CWEId']
-			vcid=i['VCId']
-			displaytext=i['DisplayText']
-			src=i['Files']['SourceFile']['File']
-			if "/" in src:
-				src_file = src.split('/')
-			elif "\\" in src:
-				src_file = src.split('\\')
-			src_file_len = len(src_file)
-			file=''.join(src_file[src_file_len-1:])
-			path=src.replace(file, '')
-			line=i['Files']['SourceFile']['Line']
-			qualifiedfunctionname=i['Files']['SourceFile']['QualifiedFunctionName']
-			functionprototype=i['Files']['SourceFile']['FunctionPrototype']
-			scope=i['Files']['SourceFile']['Scope']
-			flaws[count]={'title' : title, 'issueid' : issueid, 'severity' : severity, 'issuetype' : issuetype, 'cweid' : cweid, 'vcid' : vcid, 'displaytext' : displaytext, 'file' : file, 'path' : path, 'line' : line, 'qualifiedfunctionname' : qualifiedfunctionname, 'functionprototype' : functionprototype, 'scope' : scope}
-			count=count+1
-		#
-		# Place flaws in dictonary by severity
-		#
-		for x in flaws:
-			if flaws[x]['severity'] == '5':
-				five[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
-			
-			if flaws[x]['severity'] == '4':
-				four[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
-			
-			if flaws[x]['severity'] == '3':
-				three[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
-			
-			if flaws[x]['severity'] == '2':
-				two[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
-			
-			if flaws[x]['severity'] == '1':
-				one[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
-			
-			if flaws[x]['severity'] == '0':
-				zero[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
+		if data['scan_status'] == "SUCCESS":
+			for i in data['results']['TestResults']['Issues']['Issue']:
+				title=i['Title']
+				issueid=i['IssueId']
+				severity=i['Severity']
+				issuetype=i['IssueType']
+				cweid=i['CWEId']
+				vcid=i['VCId']
+				displaytext=i['DisplayText']
+				src=i['Files']['SourceFile']['File']
+				if "/" in src:
+					src_file = src.split('/')
+					src_file_len = len(src_file)
+					file=''.join(src_file[src_file_len-1:])
+				elif "\\" in src:
+					src_file = src.split('\\')
+					src_file_len = len(src_file)
+					file=''.join(src_file[src_file_len-1:])
+				else:
+					src_file = src
+					file = src_file
+				path=src.replace(file, '')
+				line=i['Files']['SourceFile']['Line']
+				qualifiedfunctionname=i['Files']['SourceFile']['QualifiedFunctionName']
+				functionprototype=i['Files']['SourceFile']['FunctionPrototype']
+				scope=i['Files']['SourceFile']['Scope']
+				flaws[count]={'title' : title, 'issueid' : issueid, 'severity' : severity, 'issuetype' : issuetype, 'cweid' : cweid, 'vcid' : vcid, 'displaytext' : displaytext, 'file' : file, 'path' : path, 'line' : line, 'qualifiedfunctionname' : qualifiedfunctionname, 'functionprototype' : functionprototype, 'scope' : scope}
+				count=count+1
+			#
+			# Place flaws in dictonary by severity
+			#
+			for x in flaws:
+				if flaws[x]['severity'] == '5':
+					five[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
+				
+				if flaws[x]['severity'] == '4':
+					four[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
+				
+				if flaws[x]['severity'] == '3':
+					three[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
+				
+				if flaws[x]['severity'] == '2':
+					two[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
+				
+				if flaws[x]['severity'] == '1':
+					one[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
+				
+				if flaws[x]['severity'] == '0':
+					zero[x]={'title' : flaws[x]['title'], 'issueid' : flaws[x]['issueid'], 'severity' : flaws[x]['severity'], 'issuetype' : flaws[x]['issuetype'], 'cweid' : flaws[x]['cweid'], 'vcid' : flaws[x]['vcid'], 'displaytext' : flaws[x]['displaytext'], 'file' : flaws[x]['file'], 'path' : flaws[x]['path'], 'line' : flaws[x]['line'], 'qualifiedfunctionname' : flaws[x]['qualifiedfunctionname'], 'functionprototype' : flaws[x]['functionprototype'], 'scope' : flaws[x]['scope']}
+		else:
+			quit()
 
 def genXML():
 	#root tag detailedreport
